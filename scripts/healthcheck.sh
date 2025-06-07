3b600ee587414055588358a91293e8699f1aa4ac
Geert Meersman <geertmeersman+github@gmail.com>
2025-06-07 12:17:53 +0200

    fix: healthcheck

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Check if Flask web interface is up
+WEB_PORT="${WEB_PORT:-80}"
+if ! curl -sf http://localhost:$WEB_PORT >/dev/null; then
+  echo "Flask not responding on port $WEB_PORT"
+  exit 1
+fi
+
 exit 0
